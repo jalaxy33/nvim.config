8f3c8153d15f0c863ec176c0a4c7712ab8f32ee0
jalaxy33 <jalaxy1996@outlook.com>
2026-05-12 15:32:25 +0800

    update bash/zsh indent

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,8 +1,16 @@
 return {
     "stevearc/conform.nvim",
     opts = {
+        -- define formatters
         formatters_by_ft = {
             zsh = { "shfmt" },
+        },
+        -- Customize formatters
+        formatters = {
+            -- set bash/zsh indent to 4 spaces
+            shfmt = {
+                append_args = { "-i", "4" },
+            },
         },
     },
 }
